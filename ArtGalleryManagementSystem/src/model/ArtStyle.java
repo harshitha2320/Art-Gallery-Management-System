a9afa36c1a0ee5bda8d9c5144c9cdb9b2c96aac7
harshitha <harshitha@192.168.12.234>
2025-11-01 16:26:13 +0000

    Add ArtStyle enum to represent art styles

---
+++ ArtGalleryManagementSystem/src/model/ArtStyle.java
@@ -0,0 +1,21 @@
+//package model;
+//
+//public class ArtStyle {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
+
+package model;
+
+public enum ArtStyle {
+    ABSTRACT,
+    REALISM,
+    IMPRESSIONISM,
+    CUBISM,
+    SURREALISM,
+    EXPRESSIONISM
+}
